b0b214202c8bd832adcc2e4463ab62007a6b004a
TarikEnderes <tarik.enderes@uni-ulm.de>
2019-06-10 11:45:42 +0200

    Basic structure

---
+++ src/Game/AI.cpp
@@ -0,0 +1,13 @@
+//
+// Created by tarik on 10.06.19.
+//
+
+#include "AI.h"
+
+int evalState(){
+    int val = 0;
+
+    //TODO: Make this a useful function
+
+    return val;
+}
---
+++ src/Game/AI.h
@@ -0,0 +1,13 @@
+//
+// Created by tarik on 10.06.19.
+//
+
+#ifndef KI_AI_H
+#define KI_AI_H
+
+#include "Game.hpp"
+
+int evalState();
+
+#endif //KI_AI_H
+
