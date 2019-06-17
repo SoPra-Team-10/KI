f5e1ab5537e973a82a143fb2e4ea7ced54d9f91a
TarikEnderes <tarik.enderes@uni-ulm.de>
2019-06-17 18:54:38 +0200

    Structure and some implementation

--- src/Game/AI.h
+++ src/Game/AI.h
@@ -7,7 +7,23 @@
 
 #include "Game.hpp"
 
-int evalState();
+double evalState(const std::shared_ptr<gameModel::Environment> environment, bool isLeft);
+
+double evalTeam(const std:: shared_ptr<gameModel::Team> team,
+                const std::shared_ptr<gameModel::Environment> env);
+
+double evalSeeker(const std:: shared_ptr<gameModel::Seeker> seeker,
+                  const std::shared_ptr<gameModel::Environment> env);
+
+double evalKeeper(const std:: shared_ptr<gameModel::Keeper> keeper,
+                  const std::shared_ptr<gameModel::Environment> env);
+
+double evalChaser(const std:: shared_ptr<gameModel::Chaser> chaser,
+                  const std::shared_ptr<gameModel::Environment> env);
+
+double evalBludgers(const std::shared_ptr<gameModel::Environment> env);
+
+double getHighestGoalRate(std::shared_ptr<gameModel::Environment> env, std::shared_ptr<gameModel::Player> actor);
 
 #endif //KI_AI_H
 
